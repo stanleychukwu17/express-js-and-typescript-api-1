b97e7d1ab7dc5dd3912e0d24a64dc6c083ed0f8c
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-11 13:23:59 +0100

    working on the routes     added the user (controller, model and services) files     started working on the code for user.model.ts     started working on the code for user.service.ts     started working on the code for user.controller.ts

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
-export interface UserDocument extends mongoose.Document {
+// creates the typeScript interface for the UsersDocument
+export interface UsersDocument extends mongoose.Document {
     email: string
     name: string
     password: string
@@ -11,11 +12,37 @@ export interface UserDocument extends mongoose.Document {
     comparePassword(candidatePassword: string): Promise<boolean>
 }
 
+// creates the users_schema
 const UsersSchema = new mongoose.Schema({
     email: {type:String, required:true, unique:true},
     name: {type:String, required:true},
     password: {type:String, required:true}
 }, {timestamps: true})
 
-const Users = mongoose.model('Users', UsersSchema)
-module.exports = Users
+// hashes the password before it is saved to the database
+UsersSchema.pre("save", async function (next) {
+    let user = this as UsersDocument;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) return next();
+
+    // Random additional data
+    const salt = await bcrypt.genSalt(config.get("saltWorkFactor"));
+
+    const hash = await bcrypt.hashSync(user.password, salt);
+
+    // Replace the password with the hash
+    user.password = hash;
+
+    return next();
+});
+
+// used to compare the user's password when a user wants to login
+UsersSchema.methods.comparePassword = async function (candidatePassword: string) {
+    const user = this as UsersDocument
+
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false)
+}
+
+const Users = mongoose.model<UsersDocument>('Users', UsersSchema)
+export default Users
---
+++ src/service/user.service.ts
@@ -0,0 +1,32 @@
+import { DocumentDefinition, FilterQuery } from "mongoose";
+import { omit } from "lodash";
+import Users, { UsersDocument } from "../model/user.model";
+
+export async function createUser(input: DocumentDefinition<UsersDocument>) {
+    try {
+        return await Users.create(input);
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
+
+export async function findUser(query: FilterQuery<UsersDocument>) {
+    return Users.findOne(query).lean();
+}
+
+
+type validatePasswordProps = {
+    email: UsersDocument["email"]
+    password: UsersDocument["password"] | string
+}
+export async function validatePassword({email,password}: validatePasswordProps) {
+    const user = await Users.findOne({ email });
+
+    if (!user) { return false; }
+
+    const isValid = await user.comparePassword(password);
+
+    if (!isValid) { return false; }
+
+    return omit(user.toJSON(), "password");
+}
