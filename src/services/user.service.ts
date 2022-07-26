e1c57c43620c3e7ec97412cd1bfc522eaeb7162e
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-26 17:24:27 +0100

    created the session model for mongodb

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,10 @@
 import { DocumentDefinition, FilterQuery } from "mongoose";
 import { omit } from "lodash";
-import Users, {userDocument} from "../model/user.model";
+import Users, {UserDocument} from "../model/user.model";
 import log from "../logger";
 
 
-export async function registerNewUserService (details: DocumentDefinition<userDocument>) {
+export async function registerNewUserService (details: DocumentDefinition<UserDocument>) {
     try {
         return await Users.create(details);
     } catch (err: any) {
@@ -14,6 +14,14 @@ export async function registerNewUserService (details: DocumentDefinition<userDo
 }
 
 // for creating a session to log the user in
-export async function loginUserService (details: DocumentDefinition<userDocument>) {
-    
+export async function loginUserService (details: DocumentDefinition<UserDocument>) {
+    // validate the email and password
+
+    // create a session
+
+    // create an access token
+
+    // create a refresh token
+
+    // send back refresh token and access token
 }
