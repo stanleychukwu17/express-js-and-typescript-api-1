1b31be781d4cf68f86976240bb8895fc2109bb4a
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-26 14:18:33 +0100

    brought in the database connection and the logger for errors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, {Request, Response, NextFunction} from 'express';
-// import config from 'config'
-// import log from './logger'
-// import connect from './db/connect'
+import config from 'config'
+import log from './logger'
+import connect from './db/index'
 // import routes from './routes';
 
-// const port = config.get('port') as number;
-// const host = config.get('host') as string;
+const port = config.get('port') as number;
+const host = config.get('host') as string;
 
 const app = express()
 app.use(express.json())
@@ -13,11 +13,11 @@ app.use(express.urlencoded({extended: false}))
 
 
 // setup the port so we can receive request
-// connect().then(() => {
-//     app.listen(port, () => {
-//         log.info(`port listening on http://${host}:${port}`)
-//     })
+connect().then(() => {
+    app.listen(port, () => {
+        log.info(`port listening on http://${host}:${port}`)
+    })
 
-//     // the routes handler
-//     routes(app)
-// })
+    // the routes handler
+    // routes(app)
+})
